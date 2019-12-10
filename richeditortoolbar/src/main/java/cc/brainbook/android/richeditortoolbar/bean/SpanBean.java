78f38b9b6ec62a37eeac9a9dd2b0de84b7fb8197
Robert Han <brainbook.cc@outlook.com>
2019-12-10 09:37:36 -0800

    v0.55.3#[FIX#使用parcel#marshall/unmarshall时TextBean中List<SpanBean>存取错误！]

--- richeditortoolbar/src/main/java/cc/brainbook/android/richeditortoolbar/bean/SpanBean.java
+++ richeditortoolbar/src/main/java/cc/brainbook/android/richeditortoolbar/bean/SpanBean.java
@@ -52,12 +52,13 @@ public class SpanBean<T extends Parcelable> implements Parcelable {
         spanStart = in.readInt();
         spanEnd = in.readInt();
         spanFlags = in.readInt();
-//        span = in.readParcelable(null);
-        span = in.readParcelable(getClass().getClassLoader());
-//        span = in.readParcelable(Thread.currentThread().getContextClassLoader());
-        //////??????Java中泛型得到T.class
+//        span = in.readParcelable(null);   ///IDE提示使用getClass().getClassLoader()
+        //////??????这里ClassLoader应是span的类型，由于span是泛型类，所以问题！
+        ///参考：Java中泛型得到T.class
         ///https://www.cnblogs.com/EasonJim/p/8289778.html
         ///http://www.blogjava.net/calvin/archive/2009/12/10/43830.html
+        span = in.readParcelable(getClass().getClassLoader());
+//        span = in.readParcelable(Thread.currentThread().getContextClassLoader()); ///不使用span的类型
     }
 
     public static final Creator<SpanBean> CREATOR = new Creator<SpanBean>() {
