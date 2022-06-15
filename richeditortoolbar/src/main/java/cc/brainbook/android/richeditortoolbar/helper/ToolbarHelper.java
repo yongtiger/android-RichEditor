340e510feda3433dd7a5337e7462126ec7e584fd
Robert Han <brainbook.cc@outlook.com>
2022-06-14 23:09:25 -0700

    v3.6.1#去掉[img src=...]，以OBJECT_REPLACEMENT_TEXT代替

--- richeditortoolbar/src/main/java/cc/brainbook/android/richeditortoolbar/helper/ToolbarHelper.java
+++ richeditortoolbar/src/main/java/cc/brainbook/android/richeditortoolbar/helper/ToolbarHelper.java
@@ -82,6 +82,7 @@ import cc.brainbook.android.richeditortoolbar.util.StringUtil;
 
 import static cc.brainbook.android.richeditortoolbar.config.Config.AUDIO_DRAWABLE;
 import static cc.brainbook.android.richeditortoolbar.config.Config.HEAD_SPAN_HEADING_LABELS;
+import static cc.brainbook.android.richeditortoolbar.config.Config.OBJECT_REPLACEMENT_TEXT;
 import static cc.brainbook.android.richeditortoolbar.config.Config.PLACE_HOLDER_DRAWABLE;
 import static cc.brainbook.android.richeditortoolbar.config.Config.VIDEO_DRAWABLE;
 import static cc.brainbook.android.richeditortoolbar.util.StringUtil.parseInt;
@@ -239,19 +240,23 @@ public abstract class ToolbarHelper {
                 ///字符span（带参数）：Image
                 else if (clazz == CustomImageSpan.class || clazz == VideoSpan.class || clazz == AudioSpan.class) {
                     if (start == end || spans.size() == 1) {    ///注意：不是filter之前的spans的length为1！要考虑忽略getSpans()获取的子类（不是clazz本身）
-                        final String text = String.valueOf(spannable.toString().toCharArray(), spanStart, spanEnd - spanStart);
+//                        final String text = String.valueOf(spannable.toString().toCharArray(), spanStart, spanEnd - spanStart);
+                        final String text = OBJECT_REPLACEMENT_TEXT;
 
                         final String uri = clazz == CustomImageSpan.class ? null :
                                 clazz == VideoSpan.class ? ((VideoSpan) span).getUri() : ((AudioSpan) span).getUri();
                         final String src = ((CustomImageSpan) span).getSource();
 
-                        ///从text中解析出width\height\align
-                        final String strWidth = StringUtil.getParameter(text, "width=", " ");
-                        final String strHeight = StringUtil.getParameter(text, "height=", " ");
-                        final String strAlign = StringUtil.getParameter(text, "align=", "]");
-                        final int width = parseInt(strWidth);
-                        final int height = parseInt(strHeight);
-                        final int align = strAlign == null ? ClickImageSpanDialogBuilder.DEFAULT_ALIGN : Integer.parseInt(strAlign);
+//                        ///从text中解析出width\height\align
+//                        final String strWidth = StringUtil.getParameter(text, "width=", " ");
+//                        final String strHeight = StringUtil.getParameter(text, "height=", " ");
+//                        final String strAlign = StringUtil.getParameter(text, "align=", "]");
+//                        final int width = parseInt(strWidth);
+//                        final int height = parseInt(strHeight);
+//                        final int align = strAlign == null ? ClickImageSpanDialogBuilder.DEFAULT_ALIGN : Integer.parseInt(strAlign);
+                        final int width = ((CustomImageSpan) span).getImageWidth();
+                        final int height = ((CustomImageSpan) span).getImageHeight();
+                        final int align = ((CustomImageSpan) span).getVerticalAlignment();
 
                         view.setTag(R.id.view_tag_image_text, text);
                         view.setTag(R.id.view_tag_image_uri, uri);
--- richeditortoolbar/src/main/java/cc/brainbook/android/richeditortoolbar/util/StringUtil.java
+++ richeditortoolbar/src/main/java/cc/brainbook/android/richeditortoolbar/util/StringUtil.java
@@ -3,21 +3,20 @@ package cc.brainbook.android.richeditortoolbar.util;
 import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import java.util.regex.Pattern;
 
 public abstract class StringUtil {
-    @Nullable
-    public static String getParameter(String s, String paramStart, String paramEnd) {
-        if (TextUtils.isEmpty(s) || TextUtils.isEmpty(paramStart) || !s.contains(paramStart)) {
-            return null;
-        }
-        final int index = s.indexOf(paramStart);
-        final int start = index + paramStart.length();
-        final int end = s.indexOf(paramEnd, index);
-        return s.substring(start, end);
-    }
+//    @Nullable
+//    public static String getParameter(String s, String paramStart, String paramEnd) {
+//        if (TextUtils.isEmpty(s) || TextUtils.isEmpty(paramStart) || !s.contains(paramStart)) {
+//            return null;
+//        }
+//        final int index = s.indexOf(paramStart);
+//        final int start = index + paramStart.length();
+//        final int end = s.indexOf(paramEnd, index);
+//        return s.substring(start, end);
+//    }
 
     public static boolean isUrl(@NonNull String src) {
         return src.startsWith("http://") || src.startsWith("https://");
